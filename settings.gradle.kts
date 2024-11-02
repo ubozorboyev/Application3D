06515d4a2f357b848a9ace7aa1208ddc057cc919
ulugbekbozorboev <ulugbekbozorboyev990@gmail.com>
2024-11-02 15:35:31 +0100

    asd

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Application3D"
+include(":app")
